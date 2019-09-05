64f859fe06cd8a9da1d8a32690227d7515f65ab0
Ross, James <jross1@paychex.com>
2019-09-05 17:02:21 -0400

    Decoupling overlay and content-area

--- src/components/content-area/content-area-element.ts
+++ src/components/content-area/content-area-element.ts
@@ -1,106 +1,79 @@
-import { LitElement, html, customElement, property } from 'lit-element';
+import { LitElement, customElement, css } from 'lit-element';
 import * as view from "./template.html";
 import ResizeObserver from 'resize-observer-polyfill';
 import { OverlayElement } from '../overlay/overlay-element';
 import { CaretSprite, CaretUpdate } from '../overlay/models/caret-sprite';
-const _html = html;
 
 @customElement('content-area')
 export class ContentAreaElement extends LitElement {
   
-  private _html: Function;
-  private isContentContainerReady: boolean = false;
-  private contentContainer: HTMLDivElement;
-  private overlay: OverlayElement;
-  private content: Node[];
-  private caretSprite: CaretSprite = null;
-  private resizeObserver:ResizeObserver;
+  private _content: Node[];
+  private _resizeObserver:ResizeObserver;
   private _offset_x: number;
   private _offset_y: number;
 
+  static get styles() {
+    return [ css`:host { display: block; }`];
+  }
+
   constructor(){
     super();  
-    this.resizeObserver = new ResizeObserver(this._handleNotify_resizeObserver.bind(this));
+    
+    this._resizeObserver = new ResizeObserver(this._handleNotify_resizeObserver.bind(this));
+    this._resizeObserver.observe(this);
+
+    if(!this.attributes.getNamedItem("tabindex")) {
+      this.tabIndex = 0;  
+    }
+
+    this.addEventListener("focusin",this._handleFocus.bind(this));
   }
 
   public setContent(content: Node[]){
-    this.content = content;
+    this._content = content;
 
-    if(this.isContentContainerReady) {
-      this._applyContent();
-    }
+    this.clearContent();
+    
+    this._content.forEach(this._appendNode.bind(this));
   }
 
   public clearContent(){
-    while (this.isContentContainerReady && this.contentContainer.firstChild) {
-      this.contentContainer.firstChild.remove();
+    while (this.shadowRoot.host.firstChild) {
+      this.shadowRoot.host.firstChild.remove();
     }
-  }
-
-  public firstUpdated(){
-    this.contentContainer = this.shadowRoot.getElementById('content-container') as HTMLDivElement;
-    this.overlay = this.shadowRoot.getElementById('overlay') as OverlayElement;
-    this.isContentContainerReady = true;
-
-    this.resizeObserver.observe(this.contentContainer);
-
-    if(this.content) {
-      this._applyContent();
-    }    
-  }
-
-  public render() {
-    let code: string = '_html`' + view + "`";
-
-    var result = eval(code);
-
-    return result;
   }
 
   private _handleNotify_resizeObserver(entries:Array<ResizeObserverEntry>, observer:ResizeObserver):void{
     var entry = entries[0];
     var targetRect = entry.target.getBoundingClientRect() as DOMRect;
     this._offset_x = targetRect.x;
-    this._offset_y = targetRect.y;    
+    this._offset_y = targetRect.y;   
   }
 
-  private _handleBlur_contentContainer(event: Event){
-    if(this.caretSprite) {
-      this.overlay.removeSprite(this.caretSprite);
-      this.caretSprite = null;
-    }
-  }
-
-  private _handleClick_contentContainer(event: MouseEvent){
+  private _handleFocus(event: Event){
     var selection = this.getSelection();
     var selectedNode = selection.getRangeAt(0);
     var position = selectedNode.getBoundingClientRect();
 
-    var x = position.left - this._offset_x;
-    var y = position.top - this._offset_y;
-
-    if(this.caretSprite == null) {
-      this.caretSprite = this.overlay.addCaretSprite();
+    var detail = {
+      x: position.left,
+      y: position.top,
+      height: position.height,
+      relativeX: position.left - this._offset_x,
+      relativeY: position.top - this._offset_y
     }
 
-    var update: CaretUpdate = {x: x, y: y, timeStamp: event.timeStamp, lineHeight: position.height, stroke: 2};
+    var toDispatch = new CustomEvent('caret-update',{detail:detail});
 
-    this.caretSprite.scheduleUpdate(update);
-
-    this.overlay.start();    
+    this.dispatchEvent(toDispatch);
   }
 
   public getSelection(): Selection{
     return this.shadowRoot.getSelection();
   }
 
-  private _applyContent(){
-    this.clearContent();
-    this.content.forEach(this._appendNode.bind(this));
-  }
-
   private _appendNode(node: Node) {
-    this.contentContainer.appendChild(node);
+    this.shadowRoot.appendChild(node);
   }
 
 }
--- src/components/rte/rte-element.ts
+++ src/components/rte/rte-element.ts
@@ -6,6 +6,8 @@ import ParentNode from '../../models/parent-node'
 import TextNode from '../../models/text-node';
 import DocumentNode from '../../models/document-node';
 import BlockNode from '../../models/block-node';
+import { CaretSprite, CaretUpdate } from '../overlay/models/caret-sprite';
+import { OverlayElement } from '../overlay/overlay-element';
 const _html = html;
 
 @customElement('mojj-rte')
@@ -14,8 +16,11 @@ export class RteElement extends LitElement {
   private _html: any;
 
   private contentArea: ContentAreaElement;
+  private overlay: OverlayElement;
   private renderEngine: RenderEngine;
   private root: DocumentNode;
+
+  private caretSprite:CaretSprite;
 
   constructor(){
     super();
@@ -37,14 +42,35 @@ export class RteElement extends LitElement {
 
   public firstUpdated() {
     this.contentArea = this.shadowRoot.getElementById('content-area') as ContentAreaElement;
-    var content = this.renderEngine.render(this.root);
+    this.overlay = this.shadowRoot.getElementById('overlay') as OverlayElement;
 
+    var content = this.renderEngine.render(this.root);
     this.contentArea.setContent(content.nodes);
+
+    this.contentArea.addEventListener('caret-update', this._handleEvent_caret_update.bind(this));
+
+    //this.overlay.start();
   }
 
   private _handleEvent_keydown(event: KeyboardEvent){
     event.preventDefault();
     console.log(event);
+  }
+
+  private _handleEvent_caret_update(event: CustomEvent){
+    if(this.caretSprite == null) {
+      this.caretSprite = this.overlay.addCaretSprite();
+    }
+
+    var update: CaretUpdate = {
+                                x: event.detail.relativeX,
+                                y: event.detail.relativeY,
+                                timeStamp: event.timeStamp,
+                                lineHeight: event.detail.height,
+                                stroke: 2
+                              };
+
+    this.caretSprite.scheduleUpdate(update);
   }
 
 }
