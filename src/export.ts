fc3e50aed2e249f8f0357faeaf8ff2066248db84
Ross, James <jross1@paychex.com>
2019-09-25 16:13:51 -0400

    Fixing merge issue

--- src/export.ts
+++ src/export.ts
@@ -1,5 +1,4 @@
 export { default as RteElement } from './components/rte/rte-element';
-export { default as OverlayElement } from './components/overlay/overlay-element';
 export { default as ContentAreaElement } from './components/content-area/content-area-element';
 export { default as BasicParentNode } from './core/nodes/concrete/basic-parent-node';
 export { default as BlockNode } from './core/nodes/concrete/block-node';
