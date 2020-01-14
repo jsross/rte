3064105beb40ff84bbb55b7ff0397800d4d234ed
Ross, James <jross1@paychex.com>
2020-01-14 16:56:35 -0500

    Reworking config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,35 @@
-const webpack = require('webpack');
 const path = require('path');
-const webpackMerge = require('webpack-merge');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const modeConfig = env => require(`./build-utils/webpack.${env.mode}.js`)(env);
-
-const webcomponentsjs = './node_modules/@webcomponents/webcomponentsjs';
-
-const polyfills = [
-  {
-    from: path.resolve(`${webcomponentsjs}/webcomponents-*.{js,map}`),
-    to: 'vendor',
-    flatten: true
-  },
-  {
-    from: path.resolve(`${webcomponentsjs}/bundles/*.{js,map}`),
-    to: 'vendor/bundles',
-    flatten: true
-  },
-  {
-    from: path.resolve(`${webcomponentsjs}/custom-elements-es5-adapter.js`),
-    to: 'vendor',
-    flatten: true
-  }
-];
-
-const plugins = [
-  new CleanWebpackPlugin(),
-  new webpack.ProgressPlugin(),
-  new CopyWebpackPlugin([...polyfills], {
-    ignore: ['.DS_Store']
-  })
-];
-
-module.exports = ({ mode, presets }) => {
-  return webpackMerge(
-    {
-      mode,
-      entry: './src/export.ts',
-      output: {
-        filename: '[name].js',
-        library: 'MojjRte',
-        libraryTarget: 'umd'
+module.exports = {
+  entry: './src/export',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
       },
-      devtool: 'inline-source-map',
-      module: {
-        rules: [
-          {
-            test: /\.(html)$/,
-            use: {
-              loader: 'html-loader'
-            }
-          },
-          {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-          },
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            options: {
-              plugins: ['@babel/plugin-syntax-dynamic-import'],
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    useBuiltIns: 'usage',
-                    targets: '>1%, not dead, not ie 11'
-                  }
-                ]
-              ]
-            }
-          }     
-        ]
-      },
-      plugins,
-      resolve: {
-        extensions: ['.ts', '.js'],
-        alias: {
-          '@src': path.resolve(__dirname, 'src/')
-        }
-      }
-    },
-    modeConfig({ mode, presets })
-  );
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: "html-loader?exportAsEs6Default"
+      }    
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.html'],
+    alias: {
+      '@src': path.resolve(__dirname, 'src/')
+    }
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    compress: true,
+    hot: true,
+  },
 };
