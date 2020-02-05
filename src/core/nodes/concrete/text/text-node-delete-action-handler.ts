7190b378f0ba50c6b5c220a1f6e5949c115b1765
Ross, James <jross1@paychex.com>
2020-02-05 12:36:55 -0500

    Fixes to text node delete action handler

--- src/core/nodes/concrete/text/text-node-delete-action-handler.ts
+++ src/core/nodes/concrete/text/text-node-delete-action-handler.ts
@@ -4,18 +4,22 @@ import TextNode from './text-node';
 import InsertTextAction from '@src/core/document-management/actions/insert-text-action';
 import DeleteAction from '@src/core/document-management/actions/delete-action';
 import ActionContext from '@src/core/document-management/actions/action-context';
+import HierarchyPath from '@src/core/hierarchy-path';
 
 export default class TextNodeDeleteTextActionHandler extends ActionHandler<DeleteAction, TextNode> {
     
     do(action: DeleteAction, node: TextNode, context:ActionContext): Action {
         var content = node.content;
 
-        if(action.startPath && action.startPath.depth() !== 1 || action.endPath != null && action.endPath.depth() !== 1) {
+        var startPath = action.startPath ? action.startPath : new HierarchyPath([0]);
+        var endPath = action.endPath ? action.endPath : new HierarchyPath([node.content.length]);
+
+        if(startPath.depth() !== 1 || endPath.depth() !== 1) {
             throw new Error('Bad Path');
         }
 
-        var startIndex = action.startPath ? action.startPath.head : 0;
-        var endIndex = action.endPath ? action.endPath.head : content.length - 1;
+        var startIndex = startPath.head;
+        var endIndex = endPath.head;
 
         var result:string = content.substring(0, startIndex) + content.substring(endIndex);
         var removed:string = content.substring(startIndex, endIndex);
