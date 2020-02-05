a4e4c863947af8b3bc8b81846e4bd5c1638fe379
Ross, James <jross1@paychex.com>
2020-02-05 12:37:19 -0500

    Adding method to action context

--- src/core/document-management/actions/action-context.ts
+++ src/core/document-management/actions/action-context.ts
@@ -1,4 +1,7 @@
 import ContentSelection from "@src/core/content-selection";
+import DocumentTreeNode from "@src/core/nodes/abstract/document-tree-node";
+import ActionHandler from "./action-handler";
+import RteConfig from "@src/core/config/rte-config";
 
 export default class ActionContext {
 
@@ -6,6 +9,23 @@ export default class ActionContext {
 
     constructor(selection:ContentSelection){
         this.selection = selection;
+    }
+
+    public findActionHandler(actionType:string, node: DocumentTreeNode) : ActionHandler<any,any> {
+        var nodeType = node.constructor.name;
+
+        var actionHandler = RteConfig.getRegisteredActionHandler(nodeType, actionType);
+
+        if(actionHandler != null) {
+            return actionHandler;
+        }
+
+        if(nodeType === 'DocumentTreeNode')
+            return null;
+            
+        var parentObject = Object.getPrototypeOf(node) as DocumentTreeNode;
+
+        return this.findActionHandler(actionType, parentObject);        
     }
     
 }
